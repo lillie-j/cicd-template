0eb0f20d4b455b228d677f58a904c4e6f5b0c2b4
Lillie <jacob.lillie@capgemini.com>
2025-08-26 18:04:08 +0100

    Add environment variables to main CI workflow

--- .github/actions/lint/scripts/lint_with_flake8.sh
+++ .github/actions/lint/scripts/lint_with_flake8.sh
@@ -7,10 +7,10 @@ FOLDERS="$2"
 
 source .venv/bin/activate
 if [[ "$TRUE_OR_FALSE" == "true" ]]; then
-    flake8 . --exclude="$FOLDERS,./.venv" --exit-zero
+    flake8 . --exclude="$FOLDERS,.venv" --exit-zero
 
 elif [[ "$TRUE_OR_FALSE" == "false" ]]; then
-    flake8 . --exclude="$FOLDERS,./.venv"
+    flake8 . --exclude="$FOLDERS,.venv"
 
 else
   echo "First positional argument must be 'true' or 'false'"
